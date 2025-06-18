a9f4f95e03ec50a56854da7b9ce2eacb45c928b3
SmarthVerma <smarthverma2003@gmail.com>
2025-06-18 22:29:20 +0530

    testing deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
   useEffect(() => {
     const checkMobile = () => {
       setIsMobile(window.innerWidth <= 768);
-    };
+    }; 
     
     checkMobile();
     window.addEventListener('resize', checkMobile);
