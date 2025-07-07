f9dfbb2e1335687c4d983958a44bf558948f76e9
sonal <dev.sonalgrover@gmail.com>
2025-07-07 18:11:14 +0530

    increased the text size

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -320,7 +320,7 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal"
     >
       <img src="/logo.png" alt="logo" width={30} height={30} />
-      <span className="font-medium text-white">Teqaction</span>
+      <span className="font-medium text-3xl text-white">Teqaction</span>
     </a>
   );
 };
